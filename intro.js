08f583b866b4b138a53fa7cbe881091d501df76a
m4manish-web <m4manish2354@gmail.com>
2023-11-25 12:25:21 +0530

    nested loop

---
+++ intro.js
@@ -0,0 +1,67 @@
+// var a="manish"
+// var b="Manish"
+// var c=(a==b)
+// if(c)
+// {
+//     console.log("same")
+// }
+// else
+// {
+//     console.log("different")
+// }
+// var a=10
+// var b=12
+// if(a>b)
+// {
+//     console.log("a is greater")
+// }
+// else
+// {
+//     console.log("a is smaller")
+// }
+// var bill_amount=7999
+// var discount_start_total_bill=6000
+// if(bill_amount>discount_start_total_bill)
+// {
+//     console.log("congrats you are eligible for discount")
+// }
+// else{
+//     console.log("sorry discount not available")
+// }
+// var bill_amount=5000
+// if(bill_amount>=4000){
+//     console.log("eligible for 15% discount")
+// }
+// else if(bill_amount>=3000){
+//     console.log("eligible for 10% discount")
+// }
+// else{
+//     console.log("not eligible for discount")
+// }
+// var milk_available=false
+// var butter_available=true
+// var cheese_available=true
+// if(milk_available){
+//     console.log("buy custard powder")
+// }
+// else if(butter_available){
+//     console.log("buy butter")
+// }
+// else if(cheese_available){
+//     console.log("buy cheese")
+// }
+// else{
+//     console.log("nothing is available")
+// }
+var milk_available=false
+var butter_available=true
+var cheese_available=true
+if(milk_available){
+    console.log("buy custard powder")
+}
+if(butter_available){
+    console.log("buy butter")
+}
+if(cheese_available){
+    console.log("buy cheese")
+}
